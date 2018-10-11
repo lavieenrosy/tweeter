a53ef552ace3e2959954f26901fc4171d2e42e54
Rosy Lee <rosyhlee@gmail.com>
2018-10-11 19:10:55 +0000

    completed form validation using jQuery and AJAX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,56 +6,12 @@
 
 $(document).ready(function() {
 
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-  //         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-  //         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-  //       },
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-  //         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-  //         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-  //       },
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Johann von Goethe",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-  //         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-  //         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-  //       },
-  //       "handle": "@johann49"
-  //     },
-  //     "content": {
-  //       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-  //     },
-  //     "created_at": 1461113796368
-  //   }
-  // ];
+  //hide form validation error messages
 
+  $('.form-validation').hide();
 
   //returns a tweet <article> element containing the entire HTML structure of the tweet
-
+// function createTweetElement({ user: { avatars, handle }, content, created_at }) {
   function createTweetElement(tweet) {
 
     //create header tree
@@ -104,13 +60,14 @@ $(document).ready(function() {
 
 $('.new-tweet form').on('submit', function (event) {
   event.preventDefault();
-  let newTweet = $('.new-tweet textarea').val();
+  $('.form-validation').hide();
+  let newTweet = $(this).find('textarea').val();
   if (newTweet === "") {
-    alert("Please enter a tweet!");
+    $('.form-validation').text('Please enter a tweet').slideDown("medium");
   } else if (newTweet.length > 140) {
-    alert("Your tweet must be less than 140 characters.")
+    $('.form-validation').text('Your tweet must be less than 140 characters').slideDown("medium");
   } else {
-    let data = $(event.target).serialize();
+    let data = $(this).serialize();
     $.ajax('/tweets', { method: 'POST', data: data})
     .then(function () {
       console.log('Success!', data);
@@ -119,25 +76,25 @@ $('.new-tweet form').on('submit', function (event) {
   }
 });
 
-//Responsible for fetching tweets from the /tweets page and receiving the array of tweets as JSON
+  //Responsible for fetching tweets from the /tweets page and receiving the array of tweets as JSON
 
-function loadTweets() {
-  $.ajax('/tweets', { method: 'GET'})
-  .then(function (data) {
-    renderTweets(data);
+  function loadTweets() {
+    $.ajax('/tweets', { method: 'GET'})
+    .then(function (data) {
+      renderTweets(data);
+    });
+  }
+
+  loadTweets();
+
+  //Toggle the Compose Tweet section upon clicking "Compose"
+
+  $('.compose').on('click', function (event) {
+    $('.new-tweet').slideToggle("400", function() {
+      $('.new-tweet textarea').focus();
+      console.log("Slide complete");
+    });
   });
-}
-
-loadTweets();
-
-//Toggle the Compose Tweet section upon clicking "Compose"
-
-$('.compose').on('click', function (event) {
-  $('.new-tweet').slideToggle("400", function() {
-    $('.new-tweet textarea').focus();
-    console.log("Slide complete");
-  });
-});
 
 });
 
