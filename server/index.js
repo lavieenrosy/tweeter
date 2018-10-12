d815c545473df0e427b81f956c7c98b1caf0c161
Rosy Lee <rosyhlee@gmail.com>
2018-10-12 22:02:55 +0000

    Small edits to make code more readable throughout project

--- server/index.js
+++ server/index.js
@@ -15,7 +15,6 @@ app.use(express.static("public"));
 const MongoClient   = require("mongodb").MongoClient;
 const MONGODB_URI   = "mongodb://localhost:27017/tweeter";
 
-
 MongoClient.connect(MONGODB_URI, (err, db) => {
   if (err) {
     console.error(`Failed to connect: ${MONGODB_URI}`);
